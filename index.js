ad424cfb20d858d8345237c29bc7bb568c5f34e9
wen911119 <wen911119@gmail.com>
2018-06-06 00:57:52 +0800

    1.0

---
+++ index.js
@@ -0,0 +1,38 @@
+export default function mutation(obj) {
+  return createProxy(obj)
+}
+
+function createProxy(obj) {
+  let point = obj
+  function setHandler(target, property, value) {
+    target[property] = value
+    point = obj
+  }
+  function getHandler(target, property) {
+    if (target instanceof Array && isNaN(parseInt(property))) {
+      // 访问数组方法
+      return function() {
+        point[property](...arguments)
+        point = obj
+      }
+    } else if (target[property] instanceof Array) {
+      point[property] = Array.from(point[property])
+      point = point[property]
+      return new Proxy(target[property], {
+        get: getHandler,
+        set: setHandler
+      })
+    } else {
+      point[property] = { ...point[property] }
+      point = point[property]
+      return new Proxy(target[property], {
+        get: getHandler,
+        set: setHandler
+      })
+    }
+  }
+  return new Proxy(obj, {
+    get: getHandler,
+    set: setHandler
+  })
+}
